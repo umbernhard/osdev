fd1b11b49c543f8da681218cbbfc47594baab8fa
Matt Bernhard <umbernhard@gmail.com>
2025-09-27 19:50:33 -0400

    Newline support

--- meaty-skeleton/kernel/arch/i386/tty.c
+++ meaty-skeleton/kernel/arch/i386/tty.c
@@ -40,8 +40,15 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
+	bool newline = false;
+
+	if (c != '\n') {
+			terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+	} else {
+		newline = true;
+	}
+
+	if (++terminal_column == VGA_WIDTH || newline) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
